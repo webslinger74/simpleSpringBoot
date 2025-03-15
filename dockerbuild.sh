004da0b459bc16ddd125f6e8656042015f63a116
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2025-03-15 20:43:35 +0000

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,7 @@
 echo 'creating image from dockerFile'
 
-sh 'docker build --tag stevens-react-in-docker .'
+docker build --tag stevens-react-in-docker .
 
 echo 'starting container'
 
-sh 'docker run -p 8081:8081 -p 443:443 stevens-react-in-docker'
+docker run -p 8081:8081 -p 443:443 stevens-react-in-docker
